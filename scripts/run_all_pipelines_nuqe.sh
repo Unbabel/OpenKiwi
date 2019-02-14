f0e3bccec2359b5528fd3715692053f83412f340
Miguel Vera <miguel.vera@unbabel.com>
2019-02-14 13:37:53 +0000

    Use correct embeddings for lv and cs and fix loading NuQE source model

--- scripts/run_all_pipelines_nuqe.sh
+++ scripts/run_all_pipelines_nuqe.sh
@@ -5,7 +5,11 @@
 GPU=0
 MODEL="nuqe"
 
+<<<<<<< HEAD
 LANGUAGE_PAIR="en_lv.smt"
+=======
+LANGUAGE_PAIR="de_en.smt"
+>>>>>>> 1548995... Use correct embeddings for lv and cs and fix loading NuQE source model
 DATASET="WMT18/word_level/${LANGUAGE_PAIR}"
 DATASET_NAME="wmt18"
 FORMAT="wmt18"
@@ -40,6 +44,7 @@ do
         PREDICT_DIR="${OUTPUT_MODEL_DIR}/${PREDICT_RUN_DIR_NAME}"
 
         echo "================================================================="
+<<<<<<< HEAD
         if ${RUN_JACKKNIFE}
         then
             if [[ ! -d "${JACKKNIFE_DIR:+$JACKKNIFE_DIR/}" ]]
@@ -54,8 +59,28 @@ do
             else
                 echo "Skipping jackknifing; found ${JACKKNIFE_DIR}"
             fi
+=======
+        if ${RUN_JACKKNIFE} && [[ ! -d "${JACKKNIFE_DIR:+$JACKKNIFE_DIR/}" ]]; then
+            python -m kiwi jackknife --train-config experiments/nuqe/${DATASET_NAME}.${LANGUAGE_PAIR}/train-${SIDE}.yaml \
+                                     --experiment-name "Official run for OpenKiwi" \
+                                     --splits 10 \
+                                     --seed ${SEED} \
+                                     --gpu-id ${GPU} \
+                                     --output-dir ${JACKKNIFE_DIR}
+            cp ${JACKKNIFE_DIR}/train*tags ${OUTPUT_PREDICTIONS_DIR}
+>>>>>>> 1548995... Use correct embeddings for lv and cs and fix loading NuQE source model
         else
             echo "RUN_JACKKNIFE is false"
+        fi
+
+        if [[ $SIDE = "target" ]]
+        then
+            TAGS_FILE="tags"
+        elif [[ ${SIDE} = "gaps" ]]
+        then
+            TAGS_FILE="gap_tags"
+        else
+            TAGS_FILE="source_tags"
         fi
 
         # Train
@@ -71,9 +96,15 @@ do
             echo "Skipping training; found ${TRAIN_DIR}"
         fi
 #            cp ${TRAIN_DIR}/epoch_*/${TAGS_FILE} ${OUTPUT_PREDICTIONS_DIR}/dev.${TAGS_FILE}
+<<<<<<< HEAD
         cp ${TRAIN_DIR}/epoch_*/tags ${OUTPUT_PREDICTIONS_DIR}/dev.tags 1> /dev/null 2>&1
         cp ${TRAIN_DIR}/epoch_*/gap_tags ${OUTPUT_PREDICTIONS_DIR}/dev.gap_tags 1> /dev/null 2>&1
         cp ${TRAIN_DIR}/epoch_*/source_tags ${OUTPUT_PREDICTIONS_DIR}/dev.source_tags 1> /dev/null 2>&1
+=======
+        if [[ -f "${TRAIN_DIR}/epoch_*/tags" ]]; then cp ${TRAIN_DIR}/epoch_*/tags ${OUTPUT_PREDICTIONS_DIR}/dev.tags; fi
+        if [[ -f "${TRAIN_DIR}/epoch_*/gap_tags" ]]; then cp ${TRAIN_DIR}/epoch_*/gap_tags ${OUTPUT_PREDICTIONS_DIR}/dev.gap_tags; fi
+        if [[ -f "${TRAIN_DIR}/epoch_*/source_tags" ]]; then cp ${TRAIN_DIR}/epoch_*/source_tags ${OUTPUT_PREDICTIONS_DIR}/dev.source_tags; fi
+>>>>>>> 1548995... Use correct embeddings for lv and cs and fix loading NuQE source model
 
         # Predict
         echo "================================================================="
@@ -81,9 +112,15 @@ do
                                --experiment-name "Official run for OpenKiwi" \
                                --load-model ${TRAIN_DIR}/best_model.torch \
                                --output-dir ${PREDICT_DIR}
+<<<<<<< HEAD
         cp ${PREDICT_DIR}/tags ${OUTPUT_PREDICTIONS_DIR}/test.tags 1> /dev/null 2>&1
         cp ${PREDICT_DIR}/gap_tags ${OUTPUT_PREDICTIONS_DIR}/test.gap_tags 1> /dev/null 2>&1
         cp ${PREDICT_DIR}/source_tags ${OUTPUT_PREDICTIONS_DIR}/test.source_tags 1> /dev/null 2>&1
+=======
+        if [[ -f "${PREDICT_DIR}/epoch_*/tags" ]]; then cp ${PREDICT_DIR}/epoch_*/tags ${OUTPUT_PREDICTIONS_DIR}/test.tags; fi
+        if [[ -f "${PREDICT_DIR}/epoch_*/gap_tags" ]]; then cp ${PREDICT_DIR}/epoch_*/gap_tags ${OUTPUT_PREDICTIONS_DIR}/test.gap_tags; fi
+        if [[ -f "${PREDICT_DIR}/epoch_*/source_tags" ]]; then cp ${PREDICT_DIR}/epoch_*/source_tags ${OUTPUT_PREDICTIONS_DIR}/test.source_tags; fi
+>>>>>>> 1548995... Use correct embeddings for lv and cs and fix loading NuQE source model
 
     done
 
@@ -141,3 +178,4 @@ then
 fi
 python scripts/stack_probabilities_for_linear.py -o ${OUTPUT_PREDICTIONS_ROOT_DIR}/dev.${MODEL}.stacked ${OUTPUT_PREDICTIONS_ROOT_DIR}/${ALL_RUNS}/dev.tags
 python scripts/stack_probabilities_for_linear.py -o ${OUTPUT_PREDICTIONS_ROOT_DIR}/test.${MODEL}.stacked ${OUTPUT_PREDICTIONS_ROOT_DIR}/${ALL_RUNS}/test.tags
+
