8c0da9fa5935904eb225f993a5be7668d85b9b8f
Miguel Vera <Miguel.vera@unbabel.com>
2019-12-17 22:33:51 +0000

    Fix formating error with parameter logging (#51)

--- kiwi/lib/train.py
+++ kiwi/lib/train.py
@@ -187,7 +187,9 @@ def run(ModelClass, output_dir, pipeline_options, model_options):
 
     logger.info(str(trainer.model))
     logger.info("{} parameters".format(trainer.model.num_parameters()))
-    tracking_logger.log_param("model_parameters", trainer.model.num_parameters())
+    tracking_logger.log_param(
+        "model_parameters", trainer.model.num_parameters()
+    )
 
     # Dataset iterators
     train_iter = build_bucket_iterator(
