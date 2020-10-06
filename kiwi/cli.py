e791df8501de1d3f72e1208c9f4d19626ac562a1
Daan van Stigt <daan.stigt@unbabel.com>
2020-10-06 16:15:36 +0100

    Return removed header

--- kiwi/cli.py
+++ kiwi/cli.py
@@ -1,3 +1,19 @@
+#  OpenKiwi: Open-Source Machine Translation Quality Estimation
+#  Copyright (C) 2020 Unbabel <openkiwi@unbabel.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 """
 Kiwi runner
 ~~~~~~~~~~~
@@ -45,7 +61,7 @@ from hydra._internal.hydra import Hydra
 from omegaconf import OmegaConf
 
 from kiwi import __version__
-from kiwi.lib import evaluate, predict, pretrain, search, train
+from kiwi.lib import evaluate, predict, pretrain, train, search
 
 
 def arguments_to_configuration(arguments: Dict) -> Dict:
